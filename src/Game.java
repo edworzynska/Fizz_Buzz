ff060873d2880c0a287ba715d5ebc881f70f5e5a
edworzynska <ewa.dworzynska@icloud.com>
2024-09-20 14:39:25 +0100

    fizz_buzz initial commit

---
+++ src/Game.java
@@ -0,0 +1,40 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Game {
+    public static void fizz_Buzz() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Welcome to FizzBuzz!\n*Type 0 to exit*\n\nEnter the number:");
+
+        try {
+            fizz_Buzz_Loop();
+        }
+        catch (InputMismatchException e){
+            System.out.println("Input is not a valid number! Try again: ");
+            fizz_Buzz_Loop();
+        }
+
+        System.out.println("Goodbye!");
+    }
+    public static void fizz_Buzz_Loop(){
+        Scanner userInput = new Scanner(System.in);
+        int number = userInput.nextInt();
+
+        while (number != 0){
+            try {
+                String say = "";
+                if (number % 3 == 0) say += "Fizz";
+                if (number % 5 == 0) say += "Buzz";
+                System.out.println(say.isEmpty() ? Integer.toString(number) : say);
+                System.out.println("\nEnter the number:");
+                number = userInput.nextInt();
+            }
+            catch (InputMismatchException e){
+//                throw new InputMismatchException();
+                System.out.println("Input is not a valid number! Try again: ");
+                fizz_Buzz_Loop();
+            }
+        }
+        System.out.println("Goodbye!");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+//        Say Fizz if the number is divisible by 3.
+//        Say Buzz if the number is divisible by 5.
+//        Say FizzBuzz if the number is divisible by both 3 and 5.
+//        Return the number itself, if the number is not divisible by 3 and 5.
+        Game.fizz_Buzz();
+
+    }
+}
